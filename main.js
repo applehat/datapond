c626974a590d17729f3f712d94ad8fda511db085
Cameron Chunn <cameronchunn@gmail.com>
2022-01-08 20:45:51 -0600

    eslint

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 import express from 'express'
-import * as fs from 'fs';
+import * as fs from 'fs'
 import groq from 'groq-js'
-import { nanoid } from 'nanoid'
+import {nanoid} from 'nanoid'
 
 // make sure the datastore file exists on launch
 try {
-    fs.statSync('./dataStore.json');
+  fs.statSync('./dataStore.json')
 } catch (err) {
-    // If we don't have a dataset, lets build a simple one we can query against
-    const defaultData = [
-      {_type: "item", _id: nanoid(), name: "Milk", description: "Whole"},
-      {_type: "item", _id: nanoid(), name: "Milk", description: "2% Fat"},
-      {_type: "item", _id: nanoid(), name: "Tomatoes", description: "Green cherry tomatoes"},
-      {_type: "item", _id: nanoid(), name: "Tomatoes", description: "Red cherry tomatoes"},
-      {_type: "other", _id: nanoid(), note: "This is a different document type"},
-    ]
-    fs.writeFileSync('./dataStore.json', JSON.stringify(defaultData, null, 2))
+  // If we don't have a dataset, lets build a simple one we can query against
+  const defaultData = [
+    {_type: 'item', _id: nanoid(), name: 'Milk', description: 'Whole'},
+    {_type: 'item', _id: nanoid(), name: 'Milk', description: '2% Fat'},
+    {_type: 'item', _id: nanoid(), name: 'Tomatoes', description: 'Green cherry tomatoes'},
+    {_type: 'item', _id: nanoid(), name: 'Tomatoes', description: 'Red cherry tomatoes'},
+    {_type: 'other', _id: nanoid(), note: 'This is a different document type'},
+  ]
+  fs.writeFileSync('./dataStore.json', JSON.stringify(defaultData, null, 2))
 }
 
 // trust we never make mistakes
-const dataStore = JSON.parse(fs.readFileSync('./dataStore.json', 'utf8'));
+const dataStore = JSON.parse(fs.readFileSync('./dataStore.json', 'utf8'))
 
 /**
  * Write the dataStore to the file system
@@ -27,24 +27,24 @@ const dataStore = JSON.parse(fs.readFileSync('./dataStore.json', 'utf8'));
 const writeDataStore = () => {
   console.log(`Saving dataStore to disk`)
   try {
-    fs.writeFile('./dataStore.json', JSON.stringify(dataStore, null, 2), (err)=>{
+    fs.writeFile('./dataStore.json', JSON.stringify(dataStore, null, 2), (err) => {
       if (err) {
         console.error(err)
       }
       console.log(`Datastore saved to disk`)
     })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
 /**
  * Create a URL friendly ID and make sure its not already in use.
- * @returns string
+ * @return {string}
  */
 const cleanId = () => {
   const id = nanoid()
-  if (dataStore.find(item => item._id === id)) {
+  if (dataStore.find((item) => item._id === id)) {
     // this could become a performance issue on a massive dataset...
     return cleanId()
   } else {
@@ -55,11 +55,12 @@ const cleanId = () => {
 const app = express()
 const port = 3008 // you so 2000 and late...
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 
 app.get('/documents', async (req, res) => {
-  const { query } = req.query
+  const {query} = req.query
   if (!query) {
     res.send({error: 'Please provide a query.'})
     return
@@ -68,7 +69,7 @@ app.get('/documents', async (req, res) => {
     // parse the GROQ query
     const tree = groq.parse(query)
     // evaluate the query against the data set
-    const value = await groq.evaluate(tree, { dataset: dataStore })
+    const value = await groq.evaluate(tree, {dataset: dataStore})
     // get the results
     const result = await value.get()
     res.send({result})
@@ -78,17 +79,17 @@ app.get('/documents', async (req, res) => {
 })
 
 app.post('/documents', async (req, res) => {
-  let id = cleanId()
-  const { body } = req
+  const id = cleanId()
+  const {body} = req
   if (Object.keys(body).length === 0) {
     res.send({error: `No data was provided to create a new document.`})
-    return 
+    return
   }
   if (!body._type) {
     res.send({error: `Please provide a _type for the document.`})
-    return 
+    return
   }
-  const newDocument = { ...body, _id: id }
+  const newDocument = {...body, _id: id}
   dataStore.push(newDocument)
   writeDataStore()
   res.send({success: `Document with id ${id} was created.`, document: newDocument})
@@ -96,15 +97,15 @@ app.post('/documents', async (req, res) => {
 
 app.patch('/documents/:id', async (req, res) => {
   const id = req.params.id
-  const { body } = req
-  const find = dataStore.findIndex(doc => doc._id === id)
+  const {body} = req
+  const find = dataStore.findIndex((doc) => doc._id === id)
   if (find === -1) {
     res.send({error: `No document with id ${id} was found to update.`})
-    return 
+    return
   }
   if (Object.keys(body).length === 0) {
     res.send({error: `No data was provided to update the document with id ${id}.`})
-    return 
+    return
   }
   // lets do the bare minimum to keep the data sane
   delete body._id // don't allow a new id to be set
@@ -119,10 +120,10 @@ app.patch('/documents/:id', async (req, res) => {
 
 app.delete('/documents/:id', async (req, res) => {
   const id = req.params.id
-  const find = dataStore.findIndex(doc => doc._id === id)
+  const find = dataStore.findIndex((doc) => doc._id === id)
   if (find === -1) {
     res.send({error: `No document with id ${id} was found to delete.`})
-    return 
+    return
   }
   dataStore.splice(find, 1)
   writeDataStore()
